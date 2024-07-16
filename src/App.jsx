3faa0678f60d61dd50c29a722ccddab345b5bca9
mila811230 <bluegalaxy1230@gmail.com>
2024-07-16 17:01:32 +0900

    react_firstday

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import {ColoredMessage} from './components/ColoredMessage'
+import {ColoredMessage2} from './components/ColoredMessage2'
+
+export function App(){
+  const divStyle = {
+    border : '1px solid blue' 
+  }
+  const aaa = () => {
+    alert('강아지')
+  }
+  const redStyle = {
+    color : 'red',
+    backgroundColor : 'pink'
+  }
+  const pinkStyle = {
+    color: 'pink',
+    fontSize:'20px'
+  }
+  return (
+    <div style = {divStyle}>
+      {console.log('test')}
+      <p style={{color:'skyblue', fontSize:'32px'}}>안녕하세요</p>
+      {/* <p style={redStyle}>반갑습니다. 잘 지내시죠?</p> */}
+      <ColoredMessage color ='orange' message='잘 지내시죠?'/>
+      <ColoredMessage color = 'yellowgreen' message = 'how do you do?'/>
+      <ColoredMessage2 color = 'blue'>잘 지내나요?</ColoredMessage2>
+
+      <p style={pinkStyle}>잘지내고 있습니다.</p>  
+
+      <button onClick = {aaa}>버튼</button>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import {App} from './App';
+// const App = () => {
+//   return (
+//     <p>안녕하세요</p>
+//   )
+// }
+// function App () {
+//   const divstyle = {
+//     border : '1px solid blue' 
+//   }
+//   return (
+//     <div style={divStyle}>
+//       <p>안녕하세요</p>
+//       <p>반갑습니다</p>
+//     </div>
+//   )
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <>
+  <App/>
+  </>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
